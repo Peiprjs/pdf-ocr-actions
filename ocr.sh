ed2359805ee9d4af59c158d67ed82b41a8113ed5
Peipr <m.rocacugat@student.maastrichtuniversity.nl>
2025-06-20 21:58:08 +0200

    Update ocr.sh

--- ocr.sh
+++ ocr.sh
@@ -2,6 +2,6 @@ cd input
 ls --format=single-column --group-directories-first
 for file in `ls --format=single-column --group-directories-first`
 do
-	ocrmypdf $name "ocr.$name"
+	ocrmypdf $file "ocr.$file"
 
 done
